7880dcad10515c9a87956456882cb3a771cfc871
codeguerilla <johnrbaur@gmail.com>
2015-02-13 18:06:47 +0000

    Add watersRise method - not implemented yet

--- www/modules/board/board.js
+++ www/modules/board/board.js
@@ -3,10 +3,12 @@
 
 angular.module('board', [])
 .controller('BoardCtrl', function($scope, TILESET, PHASE, fiGameUtils, fiTurns) {
-    $scope.drawTileCards = function () {
+    var waterLevel;
+  
+    function drawTileCards () {
         var i, drawCount, drawnTile, card;
         
-        drawCount = Math.ceil($scope.waterLevel / 2) + 1;
+        drawCount = Math.ceil(waterLevel / 2) + 1;
         for (i = 0; i < drawCount; i++) {
             card = $scope.tileCards.deck.pop();
             drawnTile = _.find($scope.tiles, {"id": card});
@@ -15,19 +17,23 @@ angular.module('board', [])
                 $scope.tileCards.discard.push(card);
             }
         }
-    };
+    }
     
-    //TODO : take this off $scope
-    $scope.resetTileCardDeck = function () {
+    function resetTileCardDeck() {
         var deck = $scope.tileCards.deck;
         deck.push.apply(deck, _.shuffle($scope.tileCards.discard));
         $scope.tileCards.discard = [];
-    };
+    }
+    
+    function watersRise() {
+        waterLevel++;
+        resetTileCardDeck();
+    }
     
     $scope.startGame = function (numPlayers) {
         fiGameUtils.initGame(numPlayers);
         $scope.players = fiGameUtils.playerList;
-        $scope.waterLevel = 1;
+        waterLevel = 1;
         $scope.tiles = fiGameUtils.tiles;
         $scope.tileCards = {
             deck: _.shuffle(_.pluck(TILESET, "id")),
@@ -37,10 +43,10 @@ angular.module('board', [])
     
     $scope.$watch(function() { return fiTurns.getTurn().phase; }, function (turnPhase) {
         if (turnPhase === PHASE.FLOOD) {
-            $scope.drawTileCards();
+            drawTileCards();
             fiGameUtils.gotoNextPlayer();
         }
-    })
+    });
 })
 .directive('gameBoard', function() {
     return {
